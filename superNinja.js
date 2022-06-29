edd91393409e8400a73465e8d95efd32e6e79f44
Anthony <anthonygibson24@gmail.com>
2022-06-29 18:43:48 -0500

    Ninja class attributes and methods complete

---
+++ superNinja.js
@@ -0,0 +1,27 @@
+class Ninja {
+    constructor(username, health) {
+    this.username = username
+    this.health = health
+    this.speed = 3
+    this.strength = 3
+}
+
+    sayName() {
+        console.log(`My name is ${this.username}`)
+    }
+
+    showStats() {
+        console.log(`health: ${this.health} speed: ${this.speed} strength ${this.strength}`)
+    }
+
+    drinkSake() {
+        this.health += 10
+        console.log(`${this.username}'s health is now ${this.health}`)
+    }
+}
+
+const anthony = new Ninja('anthony', 100)
+
+anthony.drinkSake()
+anthony.showStats()
+anthony.sayName()
